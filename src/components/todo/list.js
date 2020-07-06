44a5abe2c2dea23e6b924ea675224e54d8feff83
fatemaowedah <fatema.owedah@gmail.com>
2020-07-06 10:56:04 +0300

    add lab31

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+let state;
 function TodoList (props) {
   return (
     <ListGroup as="ul">
       {props.list.map(item => (
-        <ListGroup.Item
-          className={`complete-${item.complete.toString()} li ` }
-          key={item._id}
+        !item.complete ? state = 'success' : state = 'danger',
+        <ListGroup.Item as ="li"
+          className={`complete-${item.complete.toString()}` }
+          key={item._id} onClick={() => props.handleComplete(item._id)}
+          variant = {`${state}`}
         >
-          <span onClick={() => props.handleComplete(item._id)}>
-            {item.text}  By   {item.assignee} 
-          </span>
-          </ListGroup.Item>
+          {item.text}
+        </ListGroup.Item>
       ))}
     </ListGroup>
   );
