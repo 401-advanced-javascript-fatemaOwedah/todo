471a31a6ea1c74f2625544727b3da5d2a7e90df9
fatemaowedah <fatema.owedah@gmail.com>
2020-07-06 04:39:15 +0300

    add lab31

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+let variant ='';
 function TodoList (props) {
   return (
     <ListGroup as="ul">
-      {props.list.map(item => (
+      {props.list.map(item => {
+        ! item.complete ? variant = 'success' : variant = 'danger',
         <ListGroup.Item
           className={`complete-${item.complete.toString()} li ` }
           key={item._id}
@@ -14,9 +16,8 @@ function TodoList (props) {
             {item.text}  By   {item.assignee} 
           </span>
           </ListGroup.Item>
-      ))}
+      })}
     </ListGroup>
   );
 }
-
 export default TodoList;
